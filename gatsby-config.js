ac85b83bc7342847c08ee07196ad20be322453da
Uriel Scola <uriel.gomes@topaziopartner.com.br>
2020-04-23 16:20:30 -0300

    :hammer: Testing bundle size without source images

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,13 +4,13 @@ require('dotenv').config({
 
 const plugins = [
   `gatsby-plugin-react-helmet`,
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `images`,
-      path: `${__dirname}/src/assets/images`,
-    },
-  },
+  // {
+  //   resolve: `gatsby-source-filesystem`,
+  //   options: {
+  //     name: `images`,
+  //     path: `${__dirname}/src/assets/images`,
+  //   },
+  // },
   // `gatsby-plugin-sharp`,
   // {
   //   resolve: `gatsby-transformer-sharp`,
