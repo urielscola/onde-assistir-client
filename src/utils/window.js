5e1fd87f41a7a07f5afe572d01a8a8723a756cb9
Uriel Scola <uriel.gomes@topaziopartner.com.br>
2020-06-01 10:00:30 -0300

    Fixed menu scroll at iOS

--- src/utils/window.js
+++ src/utils/window.js
@@ -1,4 +1,12 @@
 export const toggleWindowBody = value => {
-  if (value) return document.body.style.overflowY = 'hidden';
+  if (value) {
+    document.ontouchmove = function (e) {
+      e.preventDefault();
+    };
+    return document.body.style.overflowY = 'hidden';
+  }
+  document.ontouchmove = function (e) {
+    return true;
+  };
   return document.body.style.overflowY = '';
 };
